b315b3e0528310aec70b5c58c933bd0d70b08e0a
gus0660 <guillaumeroch3@gmail.com>
2024-02-05 10:07:24 +0100

    querySelector de "#img_dog"

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelector('#fetch_dog').addEventListener('click', async () => {
     let fetchResult = await fetch('https://random.dog/woof.json')
     let data = await fetchResult.json()
-    console.log(data)
+    document.querySelector('#img_dog').src = data.url
 })
