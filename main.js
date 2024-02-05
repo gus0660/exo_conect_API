93b57f6d963ad7fb2080badb435a4b86b8083d9a
gus0660 <guillaumeroch3@gmail.com>
2024-02-05 10:03:29 +0100

    await sur fetchResult dans data

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.querySelector('#fetch_dog').addEventListener('click', async () => {
     let fetchResult = await fetch('https://random.dog/woof.json')
-    let data = fetchResult.json()
+    let data = await fetchResult.json()
+    console.log(data)
 })
