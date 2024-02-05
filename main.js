4bcd722467d85fd6fbd732c1edf7ebc09c079c21
gus0660 <guillaumeroch3@gmail.com>
2024-02-05 10:10:27 +0100

    function refreshDog

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 document.querySelector('#fetch_dog').addEventListener('click', async () => {
+    
+})
+function refreshDog() {
     let fetchResult = await fetch('https://random.dog/woof.json')
     let data = await fetchResult.json()
     document.querySelector('#img_dog').src = data.url
-})
+}
